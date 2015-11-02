53ad00f10c30824deee62a85e3f90cf43ebd7423
Dan Bradham <danielbradham@gmail.com>
2015-11-02 11:21:35 -0500

    Added recursive flag to search_replace

--- cgutils/commands/search_replace.py
+++ cgutils/commands/search_replace.py
@@ -17,29 +17,48 @@ import sys
 import os
 
 
-def search_replace(search_str, replace_str, root):
+def search_replace_file(root, f, search_str, replace_str):
+    if search_str in f:
+
+        source = os.path.join(root, f)
+        dest_name = f.replace(search_str, replace_str)
+        dest = os.path.join(root, dest_name)
+
+        try:
+            os.rename(source, dest)
+            print '    {} -> {}'.format(f, dest_name)
+        except OSError:
+            print '    FAILED ', '{} -> {}'.format(f, dest_name)
+
+
+def search_replace(search_str, replace_str, root, recursive=False):
+    '''Find all files with search_str and replace it with replace_str'''
+    if not recursive:
+
+        print '\nINSIDE: ', root
+
+        for f in os.listdir(root):
+
+            if not os.path.isfile(f):
+                continue
+
+            search_replace_file(root, f, search_str, replace_str)
+
+        return
+
     for root, subdirs, files in os.walk(root):
 
         print '\nINSIDE: ', root
 
         for f in files:
-            if search_str in f:
-
-                source = os.path.join(root, f)
-                dest_name = f.replace(search_str, replace_str)
-                dest = os.path.join(root, dest_name)
-
-                try:
-                    os.rename(source, dest)
-                    print '    {} -> {}'.format(f, dest_name)
-                except OSError:
-                    print '    FAILED ', '{} -> {}'.format(f, dest_name)
+            search_replace_file(root, f, search_str, replace_str)
 
 
 @click.command()
 @click.argument('search_str')
 @click.argument('replace_str')
-def cli(search_str, replace_str):
+@click.option('--recursive', is_flag=True, help='Walk the tree below the cwd')
+def cli(search_str, replace_str, recursive=False):
     '''Find a pattern in all filenames in the current directory and replace it.
 
     Example::
@@ -53,7 +72,7 @@ def cli(search_str, replace_str):
     do_it = click.confirm(confirm_msg.format(search_str, replace_str))
 
     if do_it:
-        search_replace(search_str, replace_str)
+        search_replace(search_str, replace_str, root=os.getcwd())
 
 
 if __name__ == '__main__':
